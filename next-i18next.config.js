9bc7dd7aa984a76d50b8ba7ca770e31e807d61c8
Stephen Collins <sgcollins@outlook.com>
2023-05-01 09:54:55 -0400

    demo for next-i18next issue

---
+++ next-i18next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  i18n: {
+    defaultLocale: "en-US",
+    locales: ["en-US"],
+  },
+  // This fixes the issue with the locale not being detected in Vercel
+  // see https://github.com/i18next/next-i18next/issues/462#issuecomment-832793454
+  ...(typeof window === undefined
+    ? { localePath: path.resolve("./public/locales") }
+    : {}),
+};
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { i18n } from "next-i18next";
 
 type Data = {
-  name: string
-}
+  message: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const t = i18n?.isInitialized ? i18n.t : await i18n!.init();
+  res.status(200).json({ message: t("api") as string });
 }
