e0914922e0c5ec096456f5f69eed01e5867939fd
Stephen Collins <sgcollins@outlook.com>
2023-05-01 10:03:19 -0400

    better demo with static bits

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,13 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { i18n } from "next-i18next";
 
-type Data = {
-  message: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   const t = i18n?.isInitialized ? i18n.t : await i18n!.init();
   res.status(200).json({ message: t("api") as string });
--- src/pages/static.tsx
+++ src/pages/static.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
-import { GetServerSidePropsContext } from "next";
+import { GetStaticPropsContext } from "next";
 import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,7 +27,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <h1 className={styles.title}>{t("ssr")}</h1>
+        <h1 className={styles.title}>{t("static")}</h1>
         <h1 className={styles.title}>{apiData?.message}</h1>
         <div>
           <button onClick={getApiData}>Get API Data</button>
@@ -37,9 +37,7 @@ export default function Home() {
   );
 }
 
-export async function getServerSideProps({
-  locale,
-}: GetServerSidePropsContext) {
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
   return {
     props: {
       ...(await serverSideTranslations(locale as string, ["common"])),
